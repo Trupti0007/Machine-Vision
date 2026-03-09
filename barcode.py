0be660a38b0de30dc49708fd85df5295de68bf5b
Trupti0007 <parab969623@gmail.com>
2026-03-09 23:06:12 +0530

    Create barcode.py

---
+++ barcode.py
@@ -0,0 +1,14 @@
+import cv2
+from pyzbar.pyzbar import decode
+
+def scan_barcode(frame):
+
+    barcodes = decode(frame)
+
+    codes = []
+
+    for barcode in barcodes:
+        code = barcode.data.decode("utf-8")
+        codes.append(code)
+
+    return codes
